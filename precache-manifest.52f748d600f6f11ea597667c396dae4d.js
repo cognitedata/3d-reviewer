0e0dcb5452ae67fe71652932bf1a68dee58464e8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-19 22:44:19 +0000

    Deploy cognitedata/3d-reviewer to github.com/cognitedata/3d-reviewer.git:gh-pages

---
+++ precache-manifest.52f748d600f6f11ea597667c396dae4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "e22d39c2f32f9cebc8a5",
+    "url": "./static/js/main.9152bc78.chunk.js"
+  },
+  {
+    "revision": "975fbd0d73714d5d2a82",
+    "url": "./static/js/2.cc3dd397.chunk.js"
+  },
+  {
+    "revision": "e22d39c2f32f9cebc8a5",
+    "url": "./static/css/main.dbda5832.chunk.css"
+  },
+  {
+    "revision": "975fbd0d73714d5d2a82",
+    "url": "./static/css/2.75dfbd26.chunk.css"
+  },
+  {
+    "revision": "66c7d6299597bf7e9173b0eaf966dd51",
+    "url": "./index.html"
+  }
+];
